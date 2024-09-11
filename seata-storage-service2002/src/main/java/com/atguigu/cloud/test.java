f2051b9061cfbe17d9df54febb509c357b1af4e3
lilinji <lilinji@genetind.com>
2024-09-11 23:28:41 +0800

    新创建了我的项目

---
+++ seata-storage-service2002/src/main/java/com/atguigu/cloud/test.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud;
+
+/**
+ * @Author: Linji Li
+ * @Date: 2024/9/11 23:28
+ * @Version: v1.0.0
+ * @Description: TODO
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
